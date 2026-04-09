d75da77207becea5fdbb2fd3260a778a594d984a
Ritesh Gabale <riteshworking247@gmail.com>
2026-04-09 11:12:24 +0530

    Create 344-ReverseString

---
+++ 344-ReverseString/344-ReverseString.js
@@ -0,0 +1,19 @@
+// Last updated: 4/9/2026, 11:12:25 AM
+/**
+ * @param {character[]} s
+ * @return {void} 
+ */
+var reverseString = function(s) {
+    return s.reverse();
+
+    // let front=0;
+    // let back=s.length-1;
+
+    // while(front<back){
+    //     let temp=s[front];
+    //     s[front]=s[back]
+    //     s[back]=temp;
+    //     front++;
+    //     back--;
+    // }
+};
