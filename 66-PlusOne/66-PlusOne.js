9c371bf26b37d12c7f5986c765d25b4d3b06f565
Ritesh Gabale <riteshworking247@gmail.com>
2026-04-09 11:12:46 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.js
@@ -0,0 +1,17 @@
+// Last updated: 4/9/2026, 11:12:46 AM
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] != 9) {
+      digits[i]++;
+      return digits;
+    }
+    digits[i] = 0;
+  }
+
+  digits.unshift(1);
+  return digits;
+};
