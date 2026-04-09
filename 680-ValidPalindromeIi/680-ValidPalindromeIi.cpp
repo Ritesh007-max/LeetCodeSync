080300a4442b855c2aa4b85601c4f0796152ae0c
Ritesh Gabale <riteshworking247@gmail.com>
2026-04-09 11:12:01 +0530

    Create 680-ValidPalindromeIi

---
+++ 680-ValidPalindromeIi/680-ValidPalindromeIi.cpp
@@ -0,0 +1,30 @@
+// Last updated: 4/9/2026, 11:12:01 AM
+class Solution {
+public:
+    bool isPalindrome(string& s, int i, int j) {
+        while (i < j) {
+            if (s[i] != s[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+
+        while (i < j) {
+            if (s[i] == s[j]) {
+                i++;
+                j--;
+            } else {
+                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+            }
+        }
+
+        return true;
+    }
+};
