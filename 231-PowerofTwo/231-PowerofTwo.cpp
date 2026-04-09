569643397b57d8ae7beea1d2f283355a3d0bbc57
Ritesh Gabale <riteshworking247@gmail.com>
2026-04-09 11:41:46 +0530

    File updated at 4/9/2026, 11:41:47 AM

--- 231-PowerofTwo/231-PowerofTwo.cpp
+++ 231-PowerofTwo/231-PowerofTwo.cpp
@@ -1,13 +1,11 @@
-// Last updated: 4/9/2026, 11:32:02 AM
+// Last updated: 4/9/2026, 11:41:47 AM
 1class Solution {
 2public:
 3    bool isPowerOfTwo(int n) {
-4        for(int i=0;i<32;i++){
-5            if(pow(2,i) == n){
-6                return true;
-7            }
-8        }
-9
-10        return false;
-11    }
-12};
+4        if(n == 0) return false;
+5        if(n == 1) return true;
+6        if(n % 2 != 0) return false;
+7
+8        return isPowerOfTwo(n / 2);
+9    }
+10};
