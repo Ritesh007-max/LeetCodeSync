5bb61eacfcbb3ecd55d39eeb20b1060c067f58eb
Ritesh Gabale <riteshworking247@gmail.com>
2026-05-13 22:55:36 +0530

    Time: 1 ms (33.70%) | Memory: 20.9 MB (48.94%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool firstRowZero = false;
+        bool firstColZero = false;
+
+        // 1. Check if the first row or first column need to be zeroed later
+        for (int i = 0; i < m; i++) if (matrix[i][0] == 0) firstColZero = true;
+        for (int j = 0; j < n; j++) if (matrix[0][j] == 0) firstRowZero = true;
+
+        // 2. Use first row and column as markers for the rest of the matrix
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // 3. Zero out cells based on markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // 4. Finally, zero out the first row and column if needed
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) matrix[i][0] = 0;
+        }
+        if (firstRowZero) {
+            for (int j = 0; j < n; j++) matrix[0][j] = 0;
+        }
+    }
+};
