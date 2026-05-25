41be71a50b30c77f570a23b3cac4f6314f13428c
Ritesh Gabale <riteshworking247@gmail.com>
2026-05-25 16:56:32 +0530

    Time: 2 ms (53.52%) | Memory: 7.8 MB (11.33%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string result = "";
+
+        for(int i = 0; i < address.size(); i++) {
+            if(address[i] != '.') {
+                result.push_back(address[i]);
+            }
+            else {
+                result.push_back('[');
+                result.push_back(address[i]);   
+                result.push_back(']');
+            }
+        }
+
+        return result;
+    }
+};
