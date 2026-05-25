324eebed4bb174c23eedc7049c2f6f1d374a0709
Ritesh Gabale <riteshworking247@gmail.com>
2026-05-25 17:08:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (62.85%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string result = "";
+        int maxLength = max(word1.size(), word2.size());
+
+        for (int i = 0; i < maxLength; i++) {
+            
+            if (i < word1.size()) {
+                result.push_back(word1[i]);
+            }
+
+            if (i < word2.size()) {
+                result.push_back(word2[i]);
+            }
+        }
+
+        return result;
+    }
+};
