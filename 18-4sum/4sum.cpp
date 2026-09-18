2b0157a5021abf83dcbd16860ead60b8d8de9c14
Ritesh Gabale <211916753+Ritesh007-max@users.noreply.github.com>
2026-09-18 10:03:30 +0530

    Time: 210 ms (5.58%) | Memory: 53.3 MB (8.26%) - LeetSync

---
+++ 18-4sum/4sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        set<vector<int>> unique_quads;
+        int n = nums.size();
+        if (n < 4) return {};
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 3; ++i) {
+            for (int j = i + 1; j < n - 2; ++j) {
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        unique_quads.insert({nums[i], nums[j], nums[left], nums[right]});
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return vector<vector<int>>(unique_quads.begin(), unique_quads.end());
+    }
+};
